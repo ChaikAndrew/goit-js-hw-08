567971ab1e45334dbfc0ab4af7dfb4b5a6d2bb6b
ChaikAndrew <101956196+ChaikAndrew@users.noreply.github.com>
2022-09-09 00:26:00 +0200

    add vimeo_player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,26 @@
-
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(stopTime, 1000));
+function stopTime(timeupdate) {
+  let pauseTime = timeupdate.seconds;
+  console.log(pauseTime);
+  localStorage.setItem(STORAGE_KEY, pauseTime);
+}
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+player
+  .setCurrentTime(currentTime)
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
