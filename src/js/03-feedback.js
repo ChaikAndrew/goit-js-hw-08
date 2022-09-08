160ffdb15e515841b3be1162f96f28ab5841fb00
ChaikAndrew <101956196+ChaikAndrew@users.noreply.github.com>
2022-09-08 05:27:35 +0200

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,9 @@
 import throttle from 'lodash.throttle';
 
 const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-  input: document.querySelector('.feedback-form input'),
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('.feedback-form input'),
 };
 
 const STORAGE_KEY = 'feedback-form-state';
@@ -17,14 +17,14 @@ refs.form.addEventListener('input', throttle(onInput, 200));
 
 
 function onInput(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 
 function onFormSubmit(e) {
     e.preventDefault();
-    
+
     const emailEl = e.currentTarget.email.value;
     const messageEl = e.currentTarget.message.value;
 
@@ -32,29 +32,40 @@ function onFormSubmit(e) {
         Swal.fire({
             title: 'Упс...',
             text: 'Ви маєте ввести дані в поля: Email та Message!',
+            imageUrl: 'https://fs39.fex.net/preview/4165616703/0x0',
+            imageWidth: 430,
+            imageHeight: 200,
+            backdrop: 'rgba(0,0,0,.8)',
         })
         return
-    } 
+    }
+    else {
+        Swal.fire({
+            title: 'Дякуємо :)',
+            text: 'Вашe повідомлення успішно відправлено!',
+            backdrop: 'rgba(0,0,0,.8)',
+        })
+    }
 
 
-  JSON.parse(localStorage.getItem(STORAGE_KEY));
+    JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-  console.log(formData);
-  e.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
-  formData = {};
+    console.log(formData);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    formData = {};
 }
 
 function populateTextarea() {
-  if (localStorage.getItem(STORAGE_KEY)) {
-    const savedInputs = localStorage.getItem(STORAGE_KEY);
-    formData = JSON.parse(savedInputs);
+    if (localStorage.getItem(STORAGE_KEY)) {
+        const savedInputs = localStorage.getItem(STORAGE_KEY);
+        formData = JSON.parse(savedInputs);
 
-    console.log(savedInputs);
-    refs.form.email.value = formData.email ? formData.email : '';
-    refs.form.message.value = formData.message ? formData.message : '';
-  }
+        console.log(savedInputs);
+        refs.form.email.value = formData.email ? formData.email : '';
+        refs.form.message.value = formData.message ? formData.message : '';
+    }
 }
 
-        
-        
+
+
